2e15f42f4c2651773f6b7741d6dcd5b9ed5fdd2a
rowenhama <tafadzwarhama@gmail.com>
2021-02-23 22:58:33 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AirtimeReversalComponent } from './airtime-reversal/airtime-reversal.component';
+import { AirtimeTransferComponent } from './airtime-transfer/airtime-transfer.component';
+import { RetrievePukComponent } from './retrieve-puk/retrieve-puk.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "",pathMatch:"full",redirectTo:"/airtime-transfer"},
+  {path: "airtime-reversal",component:AirtimeReversalComponent},
+  {path: "retrieve-puk",component:RetrievePukComponent},
+  {path: "airtime-transfer",component:AirtimeTransferComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
