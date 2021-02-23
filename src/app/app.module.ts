6a95b3771dc022953dbb43740a8d30f21488047d
rowenhama <tafadzwarhama@gmail.com>
2021-02-23 22:40:15 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AirtimeReversalComponent } from './airtime-reversal/airtime-reversal.component';
+import { AirtimeTransferComponent } from './airtime-transfer/airtime-transfer.component';
+import { RetrievePukComponent } from './retrieve-puk/retrieve-puk.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AirtimeReversalComponent,
+    AirtimeTransferComponent,
+    RetrievePukComponent
   ],
   imports: [
     BrowserModule,
