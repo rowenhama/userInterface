90f60741482300e48e9394b6333388c5f6f5c2ed
rowenhama <tafadzwarhama@gmail.com>
2021-02-23 22:40:35 +0200

    Create retrieve-puk.component.ts

---
+++ src/app/retrieve-puk/retrieve-puk.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-retrieve-puk',
+  templateUrl: './retrieve-puk.component.html',
+  styleUrls: ['./retrieve-puk.component.css']
+})
+export class RetrievePukComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
