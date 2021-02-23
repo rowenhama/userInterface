f8ff0311159761ab53d4250da9e11202d1605558
rowenhama <tafadzwarhama@gmail.com>
2021-02-23 22:40:09 +0200

    Create airtime-transfer.component.ts

---
+++ src/app/airtime-transfer/airtime-transfer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-airtime-transfer',
+  templateUrl: './airtime-transfer.component.html',
+  styleUrls: ['./airtime-transfer.component.css']
+})
+export class AirtimeTransferComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
