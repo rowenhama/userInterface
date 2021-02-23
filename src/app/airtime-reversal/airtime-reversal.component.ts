eb65588255aa90f74091ae0eb0d1e672d54c8b2d
rowenhama <tafadzwarhama@gmail.com>
2021-02-23 22:39:48 +0200

    Create airtime-reversal.component.ts

---
+++ src/app/airtime-reversal/airtime-reversal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-airtime-reversal',
+  templateUrl: './airtime-reversal.component.html',
+  styleUrls: ['./airtime-reversal.component.css']
+})
+export class AirtimeReversalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
